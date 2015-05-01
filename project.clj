fcb05dbfa2407b9336a9b2e219b81825fc7599b1
Frank H <fdhenard@fdhenardmbp2014.local>
2015-05-01 15:43:58 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject cledgers "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]
+                 [http-kit "2.1.18"]
+                 [hiccup "1.0.5"]
+                 [com.stuartsierra/component "0.2.3"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler cledgers.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}}
+  :main cledgers.core
+  )
---
+++ src/cledgers/core.clj
@@ -0,0 +1,58 @@
+(ns cledgers.core
+  (:require [org.httpkit.server :as http-kit-server]
+            [compojure.route :as route]
+            [compojure.handler :refer [site]]
+            [compojure.core :refer [defroutes GET POST DELETE ANY context]]
+            [com.stuartsierra.component :as component]
+            [hiccup.core :as hiccup]))
+
+;; (:use [compojure.route :only [files not-found]]
+;;       [compojure.handler :only [site]] ; form, query params decode; cookie; session, etc
+;;       [compojure.core :only [defroutes GET POST DELETE ANY context]]
+;;       org.httpkit.server)
+
+(defn show-landing-page [req] ;; ordinary clojure function, accepts a request map, returns a response map
+  ;; return landing page's html string. Using template library is a good idea:
+  ;; mustache (https://github.com/shenfeng/mustache.clj, https://github.com/fhd/clostache...)
+  ;; enlive (https://github.com/cgrand/enlive)
+  ;; hiccup(https://github.com/weavejester/hiccup)
+  (hiccup/html
+   [:h1 "Returning Server-side HTML!!!"])
+  )
+
+;; (defn update-userinfo [req]          ;; ordinary clojure function
+;;   (let [user-id (-> req :params :id)    ; param from uri
+;;         password (-> req :params :password)] ; form param
+;;     ....
+;;     ))
+
+(defroutes all-routes
+  (GET "/" [] show-landing-page)
+  ;; (GET "/ws" [] chat-handler)     ;; websocket
+  ;; (GET "/async" [] async-handler) ;; asynchronous(long polling)
+  ;; (context "/user/:id" []
+  ;;          (GET / [] get-user-by-id)
+  ;;          (POST / [] update-userinfo))
+  (route/files "/static/") ;; static file url prefix /static, in `public` folder
+  (route/not-found "<p>Page not found.</p>")) ;; all other, return 404
+
+;; (run-server (site #'all-routes) {:port 8080})
+
+(defrecord Webapp []
+  component/Lifecycle
+  (start [component]
+    (println ";; Starting Webapp")
+    (assoc component :server (http-kit-server/run-server (site #'all-routes) {:port 8080})))
+  (stop [component]
+    (println ";; Stopping Webapp")
+    (let [server (:server component)]
+      (when-not (nil? server)
+        (server :timeout 100)
+        (assoc component :server nil)))))
+
+(defn cledgers-system []
+  (component/system-map
+   :app (Webapp.)))
+
+(defn -main [& args]
+  (component/start (cledgers-system)))

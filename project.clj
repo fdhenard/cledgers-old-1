49b89db6092c199fcb1ebe8c0d6259cdd6ef04f9
Frank H <fdhenard@fdhenardmbp2014.local>
2015-05-14 12:29:42 -0500

    initial commit

--- project.clj
+++ project.clj
@@ -2,18 +2,26 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [compojure "1.3.4"]
                  [ring/ring-defaults "0.1.2"]
                  [http-kit "2.1.18"]
                  [hiccup "1.0.5"]
-                 [com.stuartsierra/component "0.2.3"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [com.stuartsierra/component "0.2.3"]
+                 [org.clojure/clojurescript "0.0-3269"]
+                 [reagent "0.5.0"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-cljsbuild "1.0.6"]]
   :ring {:handler cledgers.handler/app}
-  :profiles
-  {:dev {:source-paths ["dev"]
-         :dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]
-                        [ring/ring-devel "1.3.2"]]}}
+  :profiles  {:dev {:source-paths ["dev"]
+                    :dependencies [[javax.servlet/servlet-api "2.5"]
+                                   [ring-mock "0.1.5"]
+                                   [ring/ring-devel "1.3.2"]]}}
+  :cljsbuild {:builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "app.js"
+                                   :optimizations :whitespace
+                                   ;;#_:preamble ["reagent/react.js"]
+                                   :pretty-print true}}]}
+  :hooks [leiningen.cljsbuild]
   :main cledgers.core
   )
---
+++ src-cljs/cledgers/core.cljs
@@ -0,0 +1,46 @@
+(ns cledgers.core
+  (:require [reagent.core :as r]))
+
+(enable-console-print!)
+
+(def app-state (r/atom {:transactions
+                        [
+                         {:key 1 :payee "Erik Swanson" :amount 27.50}
+                         ]}))
+
+(defn update-xactions! [f & args]
+  (apply swap! app-state update-in [:transactions] f args))
+
+(defn add-xaction! [xaction]
+  (update-xactions! conj xaction))
+
+(defn remove-xaction! [xaction]
+  (update-xactions! (fn [xactions]
+                      (vec (remove #(= % xaction) xactions)))
+                    xaction))
+
+
+;; UI components
+
+(defn xaction-repr [xaction]
+  [:tr
+   [:td (:payee xaction)]
+   [:td (:amount xaction)]
+   [:td
+    [:button {:on-click #(remove-xaction! xaction)} "Delete"]]])
+
+(defn xaction-list-repr []
+  [:div
+   [:h1 "Transactions Baby"]
+   [:table
+    [:thead
+     [:tr [:td "Payee"] [:td "Amount"]]]
+    [:tbody
+     (for [xaction (:transactions @app-state)]
+       [xaction-repr xaction])]]])
+
+;; Reagent Render Root Component
+(defn start []
+  (r/render-component
+   [xaction-list-repr]
+   (.getElementById js/document "root")))
